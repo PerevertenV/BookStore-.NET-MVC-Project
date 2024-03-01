5b6149668651744a7b86c5bea1c1dc3f44767ee1
PerevertenV <pereverten.vadym@gmail.com>
2024-03-01 19:56:25 +0200

    database configuration

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "History", DisplayOrder = 3 },
+                new Category { ID = 4, Name = "Fairy Tail", DisplayOrder = 4 }
+                );
+        }
+    }
+}
